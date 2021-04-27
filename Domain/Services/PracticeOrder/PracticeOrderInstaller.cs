e13b85004a51b7b2cb4a8caadd469b1438d547a4
Ivan Barabanshikov <barabanzz871@gmail.com>
2021-04-27 12:33:39 +0300

    + add practice order provider service

---
+++ Domain/Services/PracticeOrder/PracticeOrderInstaller.cs
@@ -0,0 +1,15 @@
+﻿using course_backend.Abstractions.DI;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Domain.Services
+{
+    public class PracticeOrderInstaller: IInjectable
+    {
+        public void Inject(IServiceCollection serviceCollection, IConfiguration configuration, IHostEnvironment env)
+        {
+            serviceCollection.AddScoped<IPracticeOrderProvider, PracticeOrderProviderService>();
+        }
+    }
+}
---
+++ Domain/Services/PracticeOrder/PracticeOrderService.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Data;
+using Domain.Entity;
+using Domain.UseCases.PracticeOrder.GetOne;
+using Microsoft.EntityFrameworkCore;
+
+namespace Domain.Services
+{
+    public interface IPracticeOrderProvider
+    {
+        Task<List<PracticeOrder>> GetUserPracticeOrders(int userId, int lessonId);
+        Task<List<PracticeOrder>> GetUserPracticeOrdersAll(int userId);
+    }
+
+    public class PracticeOrderProviderService : IPracticeOrderProvider
+    {
+        private readonly IAppDbContext _context;
+
+        public PracticeOrderProviderService(IAppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<List<PracticeOrder>> GetUserPracticeOrders(int userId, int lessonId)
+        {
+            return await _context.PracticeOrders
+                .Include(x => x.Author)
+                .Where(x => x.Author.Id == userId && x.LessonId == lessonId)
+                .OrderByDescending(x => x.CreatedDate)
+                .ToListAsync();
+        }
+        
+        public async Task<List<PracticeOrder>> GetUserPracticeOrdersAll(int userId)
+        {
+            return await _context.PracticeOrders
+                .Include(x => x.Author)
+                .Where(x => x.Author.Id == userId)
+                .OrderByDescending(x => x.CreatedDate)
+                .ToListAsync();
+        }
+    }
+}
